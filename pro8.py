858f7744bd3459ef03437d94464ec1b0c91f03eb
Tinasamayasundaram <50619385+Tinasamayasundaram@users.noreply.github.com>
2019-06-10 16:50:37 +0530

    Create pro8.py

---
+++ pro8.py
@@ -0,0 +1,8 @@
+import math
+import functools
+n,k=map(int,input().split())
+l1=[int(i) for i in input().split()]
+for i in range(k):
+    c,d=map(int,input().split())
+    t=functools.reduce(math.gcd,l1[c-1:d])
+    print(t)
